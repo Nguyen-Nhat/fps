9726161d1f93c3ee3bd97f8a6325ae2e0ca34bce
Quý Trần Minh <quy.tm@teko.vn>
2022-11-26 02:59:04 +0000

    Merge: Releases/0.1.0 -> master (fix wrong file path)

--- providers/fileservice/file_service.client.go
+++ providers/fileservice/file_service.client.go
@@ -2,10 +2,12 @@ package fileservice
 
 import (
 	"crypto/tls"
+	"fmt"
 	config "git.teko.vn/loyalty-system/loyalty-file-processing/configs"
 	"git.teko.vn/loyalty-system/loyalty-file-processing/pkg/logger"
 	"git.teko.vn/loyalty-system/loyalty-file-processing/providers/utils"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -47,7 +49,11 @@ func NewClient(conf config.FileServiceConfig) *Client {
 
 func (c *Client) uploadFile(req uploadFileRequest) (uploadFileResponse, error) {
 	// 1. Build url & header
-	url := c.conf.Endpoint + c.conf.Paths.UploadDoc
+	params := url.Values{}
+	params.Add("fileName", req.FileName)
+
+	requestUrl := fmt.Sprintf("%s%s?%s", c.conf.Endpoint, c.conf.Paths.UploadDoc, params.Encode())
+	logger.Infof("===== Upload file `%s` to url %s", req.FileName, requestUrl)
 
 	// 2. Build request body
 	fileContent := utils.FileContent{
@@ -57,7 +63,7 @@ func (c *Client) uploadFile(req uploadFileRequest) (uploadFileResponse, error) {
 	}
 
 	// 3. Send http request
-	httpResp, err := utils.UploadFile[uploadFileResponse](c.client, url, fileContent)
+	httpResp, err := utils.UploadFile[uploadFileResponse](c.client, requestUrl, fileContent)
 	if err != nil {
 		logger.Errorf("===== fileservice.GetTransactionByID: Call File Service Error: %s", err.Error())
 		return uploadFileResponse{}, err
