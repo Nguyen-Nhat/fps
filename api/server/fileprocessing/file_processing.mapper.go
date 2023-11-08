4e6825fe81a338eaf4657d10675d4711ab108318
quytm <quy.tm@teko.vn>
2023-11-08 18:42:44 +0700

    SRE-3428: support `totalErrorPlus`

--- api/server/fileprocessing/file_processing.mapper.go
+++ api/server/fileprocessing/file_processing.mapper.go
@@ -3,6 +3,7 @@ package fileprocessing
 import (
 	"git.teko.vn/loyalty-system/loyalty-file-processing/api/server/common/response"
 	"git.teko.vn/loyalty-system/loyalty-file-processing/internal/fileprocessing"
+	"github.com/tidwall/gjson"
 )
 
 func fromInternalToGetFileHistoryData(fp []*fileprocessing.ProcessingFile, pagination *response.Pagination) *GetFileProcessHistoryData {
@@ -18,6 +19,8 @@ func fromInternalToGetFileHistoryData(fp []*fileprocessing.ProcessingFile, pagin
 }
 
 func toProcessHistoryFileFromEntity(pf *fileprocessing.ProcessingFile) ProcessingHistoryFile {
+	totalErrorPlus := getTotalErrorPlus(pf.FileParameters)
+
 	return ProcessingHistoryFile{
 		ClientId:            pf.ClientID,
 		ProcessingFileId:    pf.ID,
@@ -25,12 +28,34 @@ func toProcessHistoryFileFromEntity(pf *fileprocessing.ProcessingFile) Processin
 		FileUrl:             pf.FileURL,
 		ResultFileUrl:       pf.ResultFileURL,
 		Status:              mapStatus(pf.Status),
-		StatsTotalRow:       pf.StatsTotalRow,
-		StatsTotalProcessed: pf.StatsTotalProcessed,
+		StatsTotalRow:       pf.StatsTotalRow + totalErrorPlus,
+		StatsTotalProcessed: pf.StatsTotalProcessed + totalErrorPlus,
 		StatsTotalSuccess:   pf.StatsTotalSuccess,
 		ErrorDisplay:        pf.ErrorDisplay,
 		CreatedAt:           pf.CreatedAt.UnixMilli(),
 		CreatedBy:           pf.CreatedBy,
+	}
+}
+
+// totalErrorPlusField ... is created when
+// - BFF pre-processed file and there are some rows are error
+// - BFF want to give the number of error rows to FPS to store
+// - When FPS return Import History, FPS will add this number to result statistic, include fields: statsTotalRow and statsTotalProcessed
+// Related ticket: https://jira.teko.vn/browse/SRE-3428
+const totalErrorPlusField = "totalErrorPlus"
+
+// getTotalErrorPlus ... use totalErrorPlusField
+func getTotalErrorPlus(fileParameters string) int32 {
+	if len(fileParameters) == 0 {
+		return 0
+	}
+
+	res := gjson.Get(fileParameters, totalErrorPlusField)
+	totalErrorPlus := int32(res.Int())
+	if totalErrorPlus <= 0 { // ignore negative value
+		return 0
+	} else {
+		return totalErrorPlus
 	}
 }
 
